5139a22d90ba9c754d3b71344ef9d81226bd0ee0
Rocco Orefice <rocco.orefice92@gmail.com>
2020-11-30 11:32:17 +0100

    utilizzo tastiera per next e prev
    inserimento dei keydown per l'utilizzo delle freccette per andare avanti e indietro

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,52 +6,65 @@
 // Utiliziamo una classe first e last  per capire quali sono la prima e ultima immagine dello slider
 // Utilizziamo una classe active per aiutarci a capire quale è l'immagine attuale da visualizzare nello slider
 
-
-
-//click direzione "avanti"
-function goNext(){
-    var firstImg = $('img.first');
-    var activeImg = $('img.active');
-    var firstCircle = $('.fa-circle.first');
-    var activeCircle = $('.fa-circle.active');
-
-    activeImg.removeClass('active');
-    activeCircle.removeClass('active');
-
-    //uso 'last' per attribuire la direzione di --->"avanti"
-    if (activeImg.hasClass('last')){
-        firstImg.addClass('active');
-        firstCircle.addClass('active');
-    }else {
-        activeImg.next().addClass('active');
-        activeCircle.next().addClass('active');
-    }
-}
-
-//click direzione "indietro"
-function goPrev(){
-    var lastImg = $('img.last');
-    var activeImg = $('img.active');
-    var lastCircle = $('.fa-circle.last');
-    var activeCircle = $('.fa-circle.active');
+//document ready
+$(function(){
     
-    activeImg.removeClass('active');
-    activeCircle.removeClass('active');
+    //invoco le funzioni
+    $('.next .fa-angle-right').click(function(){ goNext(); });
+    $('.prev .fa-angle-left').click(function(){ goPrev(); });
 
-    
-    //uso 'first' per attribuire la direzione di --->"indietro"
-    if (activeImg.hasClass('first')){
-        lastImg.addClass('active');
-        lastCircle.addClass('active');
-    } else {
-        activeImg.prev().addClass('active');
-        activeCircle.prev().addClass('active');
+    //click direzione "avanti"
+    function goNext(){
+        var firstImg = $('img.first');
+        var activeImg = $('img.active');
+        var firstCircle = $('.fa-circle.first');
+        var activeCircle = $('.fa-circle.active');
+
+        activeImg.removeClass('active');
+        activeCircle.removeClass('active');
+
+        //uso 'last' per attribuire la direzione di --->"avanti"
+        if (activeImg.hasClass('last')){
+            firstImg.addClass('active');
+            firstCircle.addClass('active');
+        }else {
+            activeImg.next().addClass('active');
+            activeCircle.next().addClass('active');
+        }
+        
     }
-}
 
-//invoco le funzioni
-$('.next .fa-angle-right').click(function(){ goNext(); });
-$('.prev .fa-angle-left').click(function(){ goPrev(); });
+    //click direzione "indietro"
+    function goPrev(){
+        var lastImg = $('img.last');
+        var activeImg = $('img.active');
+        var lastCircle = $('.fa-circle.last');
+        var activeCircle = $('.fa-circle.active');
+        
+        activeImg.removeClass('active');
+        activeCircle.removeClass('active');
 
+        //uso 'first' per attribuire la direzione di --->"indietro"
+        if (activeImg.hasClass('first')){
+            lastImg.addClass('active');
+            lastCircle.addClass('active');
+        } else {
+            activeImg.prev().addClass('active');
+            activeCircle.prev().addClass('active');
+        }
+    }
 
-
+    //Utilizzo della tastiera per "avanti" o "indietro"
+    $(document).keydown(function (e) { 
+        switch (e.which) {
+            case 37: //freccia sinistra
+                goPrev();
+                break;
+            case 39: //freccia destra
+                goNext();
+                break;
+            default:
+                break;
+        }
+    });
+});
